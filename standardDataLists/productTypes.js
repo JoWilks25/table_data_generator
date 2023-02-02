bfccd78d5a2702c748580f67982dd1f5c1fc2630
JoWilks25 <58911477+JoWilks25@users.noreply.github.com>
2023-02-02 14:26:29 +0000

    Large cleanup (#1)

--- standardDataLists/productTypes.js
+++
@@ -1,6 +0,0 @@
-const BANKING_PRODUCT_TYPES = [
-    'Demand deposit',
-    'Term deposit'
-]
-
-export default BANKING_PRODUCT_TYPES;
--- structures/tableStructures.js
+++ structures/tableStructures.js
@@ -4,29 +4,29 @@ const PrimaryKey = 'Account Number'
 
 // AccountBalancesFields
 export const CoreDataFields = [
-    'Account Number',
-    'Location',
-    'Datetime',
-    'Account Currency',
-    'Product Type',
-    'Account Status',
-    'Financial Institution',
-    'Account Currency Balance',
-    'Base Currency Balance (GBP)',
-    'Entity'
+  'Account Number',
+  'Location',
+  'Datetime',
+  'Account Currency',
+  'Product Type',
+  'Account Status',
+  'Financial Institution',
+  'Account Currency Balance',
+  'Base Currency Balance (GBP)',
+  'Entity'
 ]
 
 // Transactions Data
 export const RelationalDataFields = [
-    'Account Number',
-    'Location',
-    'Account Currency',
-    'Financial Institution',
-    'Datetime',
-    'Transaction type',
-    'Inflows / Outflows',
-    'Entity',
-    'Transaction Amount in Account Currency',
-    'Transaction Amount in Base Currency (GBP)',
-    'Transaction Volume'
+  'Account Number',
+  'Location',
+  'Account Currency',
+  'Financial Institution',
+  'Datetime',
+  'Transaction type',
+  'Inflows / Outflows',
+  'Entity',
+  'Transaction Amount in Account Currency',
+  'Transaction Amount in Base Currency (GBP)',
+  'Transaction Volume'
 ]
