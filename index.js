e9293cc1a24297794fd653d531ff5c84a1d3b90f
Jo Wilks <jowilks@Jos-MacBook-Pro.local>
2023-02-03 13:48:45 +0000

    Fix transactions not adding up

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { selectRandValue, getDates, generateRandomBankName } from './helpers/fun
 import { SUBSET_COUNTRIES, SUBSET_CURRENCIES, BANKING_PRODUCT_TYPES } from './helpers/sampleDataLists.js'
 
 // VARIABLES
-const numberAccounts = 10
+const numberAccounts = 2
 const accountLength = 12
-const fromDate = '2022-12-01' // YYYY-MM-DD
+const fromDate = '2022-12-28' // YYYY-MM-DD
 const toDate = '2022-12-31' // YYYY-MM-DD
 const outputDateFormat = 'YYYY-MM-DD HH:mm:ss'
 
@@ -35,7 +35,7 @@ accountNosList.forEach((accountNumber) => {
     const dailyAccountBalRow = {
       ...baseAccounBalRow,
       'Datetime': date.format(outputDateFormat),
-      'Account Currency Balance': index === 0 ? randNumber({ min: 10000, max: 10000000, fraction: 2 }) : null // Set opening balance for earliest date (assuming index 0 is earliest date)
+      'Account Currency Balance': index === 0 ? randNumber({ min: 10000, max: 10000000, precision: 1000 }) : 0 // Set opening balance for earliest date (assuming index 0 is earliest date)
     }
     accountBalancesTable.push(dailyAccountBalRow)
   })
@@ -46,22 +46,34 @@ accountNosList.forEach((accountNumber) => {
 let transactionsTable = []
 
 accountNosList.forEach((accountNumber) => {
-  dates.forEach((date) => {
+  let prevDayAccBalObject
+  let prevDaysTrans
+  dates.forEach((date, index) => {
     // Generating random number of transactions for a specific account number
-    const noTransactions = randNumber({ min: 0, max: 50 })
+    const noTransactions = randNumber({ min: 0, max: 20 })
     const transactionAmounts = randNumber({ length: noTransactions, min: -100000, max: 100000, fraction: 2 })
 
     // Updating the balance in the Account Balances list so it adds up.
     const sumTransactions = transactionAmounts.reduce((prevValue, nextValue) => Number(prevValue) + Number(nextValue), [0])
 
-    let prevDayAccBalObject = accountBalancesTable.find((accBalObject) => accountNumber === accBalObject['Account Number'] && date.format(outputDateFormat) === accBalObject.Datetime)
-    let currentDayAccBalObject = accountBalancesTable.find((accBalObject) => accountNumber === accBalObject['Account Number'] && date.clone().subtract(1, 'd').format(outputDateFormat) === accBalObject.Datetime)
-    if (prevDayAccBalObject && currentDayAccBalObject) {
-      currentDayAccBalObject['Account Currency Balance'] = prevDayAccBalObject['Account Currency Balance'] + sumTransactions
+    let currentDayAccBalObject = accountBalancesTable.find((accBalObject) => {
+      return accountNumber === accBalObject['Account Number'] && date.format(outputDateFormat) === accBalObject.Datetime
+    })
+    
+    // If current is earliest date i.e. fromDate, then currentDayAccBalObject = the starting balance value of the account.
+    console.log('prevDayAccBalObject', prevDayAccBalObject)
+    if (index === 0) {
+      prevDayAccBalObject = {...currentDayAccBalObject}
+      prevDaysTrans = sumTransactions
+    } else {
+      currentDayAccBalObject['Account Currency Balance'] = prevDayAccBalObject['Account Currency Balance'] + prevDaysTrans
+      prevDayAccBalObject = {...currentDayAccBalObject}
+      prevDaysTrans = sumTransactions
     }
+
     // Restricting random times to within 9am - 6pm
-    const startDateTime = date.hours(9).format()
-    const endDateTime = date.hours(18).format()
+    const startDateTime = date.clone().hours(9).format()
+    const endDateTime = date.clone().hours(18).format()
 
     // Creating each transaction row based off the amounts
     transactionAmounts.forEach((amount) => {
@@ -81,39 +93,39 @@ accountNosList.forEach((accountNumber) => {
 })
 
 
-// // WRITE ACCOUNT BALANCES TABLE TO CSV
-// import { createObjectCsvWriter } from 'csv-writer'
-// const csvWriterAccountBalances = createObjectCsvWriter({
-//   path: 'output/account_balances.csv',
-//   header: [
-//     { id: 'Account Number', title: 'Account Number' },
-//     { id: 'Location', title: 'Location' },
-//     { id: 'Datetime', title: 'Datetime' },
-//     { id: 'Account Currency', title: 'Account Currency' },
-//     { id: 'Product Type', title: 'Product Type' },
-//     { id: 'Financial Institution', title: 'Financial Institution' },
-//     { id: 'Account Currency Balance', title: 'Account Currency Balance' },
-//     { id: 'Entity', title: 'Entity' },
-//   ]
-// });
+// WRITE ACCOUNT BALANCES TABLE TO CSV
+import { createObjectCsvWriter } from 'csv-writer'
+const csvWriterAccountBalances = createObjectCsvWriter({
+  path: 'output/account_balances.csv',
+  header: [
+    { id: 'Account Number', title: 'Account Number' },
+    { id: 'Location', title: 'Location' },
+    { id: 'Datetime', title: 'Datetime' },
+    { id: 'Account Currency', title: 'Account Currency' },
+    { id: 'Product Type', title: 'Product Type' },
+    { id: 'Financial Institution', title: 'Financial Institution' },
+    { id: 'Account Currency Balance', title: 'Account Currency Balance' },
+    { id: 'Entity', title: 'Entity' },
+  ]
+});
 
-// csvWriterAccountBalances
-//   .writeRecords(accountBalancesTable)
-//   .then(() => console.log('The Account Balances CSV file was written successfully'));
+csvWriterAccountBalances
+  .writeRecords(accountBalancesTable)
+  .then(() => console.log('The Account Balances CSV file was written successfully'));
 
-// // WRITE TRANSACTIONS TABLE TO CSV
-// const csvWriterTransactions = createObjectCsvWriter({
-//   path: 'output/transactions.csv',
-//   header: [
-//     { id: 'Account Number', title: 'Account Number' },
-//     { id: 'Datetime', title: 'Datetime' },
-//     { id: 'Transaction type', title: 'Transaction type' },
-//     { id: 'Inflows / Outflows', title: 'Inflows / Outflows' },
-//     { id: 'Transaction Amount in Account Currency', title: 'Transaction Amount in Account Currency' },
-//     { id: 'Counterparty', title: 'Counterparty' },
-//   ]
-// });
+// WRITE TRANSACTIONS TABLE TO CSV
+const csvWriterTransactions = createObjectCsvWriter({
+  path: 'output/transactions.csv',
+  header: [
+    { id: 'Account Number', title: 'Account Number' },
+    { id: 'Datetime', title: 'Datetime' },
+    { id: 'Transaction type', title: 'Transaction type' },
+    { id: 'Inflows / Outflows', title: 'Inflows / Outflows' },
+    { id: 'Transaction Amount in Account Currency', title: 'Transaction Amount in Account Currency' },
+    { id: 'Counterparty', title: 'Counterparty' },
+  ]
+});
 
-// csvWriterTransactions
-//   .writeRecords(transactionsTable)
-//   .then(() => console.log('The Transactions CSV file was written successfully'));
+csvWriterTransactions
+  .writeRecords(transactionsTable)
+  .then(() => console.log('The Transactions CSV file was written successfully'));
